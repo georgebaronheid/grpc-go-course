d0e2126bbde5e3734c71cc7c7db4d0be317d9b8e
George Baronheid <baronheid.george@gmail.com>
2021-11-29 19:28:03 -0300

    Errors implementation

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"google.golang.org/grpc"
-	"grpc-go-course/calculator/calculatorpb"
-	"io"
-	"log"
+    "context"
+    "fmt"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
+    "grpc-go-course/calculator/calculatorpb"
+    "log"
 )
 
 func main() {
@@ -26,67 +27,103 @@ func main() {
 	//if err := doClientStreaming(c); err != nil {
 	//	log.Fatalf("Error calling doClientStreaming: [ %v ]", err)
 	//}
-	doBiDirectionalStreaming(c)
+	//doBiDirectionalStreaming(c)
 
+	doSqrtWithError(c)
 }
 
-func doBiDirectionalStreaming(cSC calculatorpb.CalculatorServiceClient) {
-	fMC, err := cSC.FindMaximum(context.Background())
-	if err != nil {
-		log.Fatalf("error calling FindMaximum from Server: [ %v ]", err)
-		return
-	}
-	waitc := make(chan struct{})
+func doSqrtWithError(cSC calculatorpb.CalculatorServiceClient) {
+	var c1, c2 *calculatorpb.SquareRootResponse
+	var err error
+	//Correct calls
+	if c1, err = cSC.SquareRoot(context.Background(), &calculatorpb.SquareRootRequest{Number: 9.0}); err != nil {
+        errorHandling(err)
+    }
+    fmt.Printf("Squareroot of 9.0: [ %v ]\n", c1.GetNumberRoot())
 
-	iS := []int32{
-		1,
-		5,
-		3,
-		6,
-		2,
-		20,
-		200,
-		200,
-		2000,
-		2000,
-		2001,
-	}
-	go func() {
-		fmt.Println("Starting [ send GoRoutine ]")
-		//	Sending stream:
-		for _, n := range iS {
-			if err := fMC.Send(&calculatorpb.FindMaximumRequest{Number: n}); err != nil {
-				close(waitc)
-				log.Fatalf("error sending int to server: [ %v ]", err)
-			}
-		}
-		if err := fMC.CloseSend(); err != nil {
-			close(waitc)
-			log.Fatalf("error closeSend: [ %v ]", err)
-		}
-	}()
+    if c2, err = cSC.SquareRoot(context.Background(), &calculatorpb.SquareRootRequest{Number: 0.0}); err != nil {
+        errorHandling(err)
+    }
+    fmt.Printf("Squareroot of 0.0: [ %v ]\n", c2.GetNumberRoot())
 
-	var rIH []int32
-	go func() {
-		//	Receiving stream
-		for {
-			rI, err := fMC.Recv()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				close(waitc)
-				log.Fatalf("error recieving data: [ %v ]", err)
-			}
-			fmt.Printf("Recieved Maximum int: [ %v ]\n", rI.GetCurrentMaximum())
-			rIH = append(rIH, rI.GetCurrentMaximum())
-		}
-		close(waitc)
-	}()
-	<-waitc
 
-	fmt.Printf("All maximums were: %v\n", rIH)
+    //Incorrect call
+    if _, err = cSC.SquareRoot(context.Background(), &calculatorpb.SquareRootRequest{Number: -9.0}); err != nil {
+        errorHandling(err)
+    }
 }
+
+func errorHandling(err error) {
+    respErr, ok := status.FromError(err)
+    if ok {
+        //    Known error
+        fmt.Println(respErr.Code())
+        fmt.Println(respErr.Message())
+        if respErr.Code() == codes.InvalidArgument {
+            fmt.Println("We probably sent an incorrect number")
+        }
+    } else {
+        log.Fatalf("Unknow error [ %v ]", err)
+    }
+}
+
+//func doBiDirectionalStreaming(cSC calculatorpb.CalculatorServiceClient) {
+//	fMC, err := cSC.FindMaximum(context.Background())
+//	if err != nil {
+//		log.Fatalf("error calling FindMaximum from Server: [ %v ]", err)
+//		return
+//	}
+//	waitc := make(chan struct{})
+//
+//	iS := []int32{
+//		1,
+//		5,
+//		3,
+//		6,
+//		2,
+//		20,
+//		200,
+//		200,
+//		2000,
+//		2000,
+//		2001,
+//	}
+//	go func() {
+//		fmt.Println("Starting [ send GoRoutine ]")
+//		//	Sending stream:
+//		for _, n := range iS {
+//			if err := fMC.Send(&calculatorpb.FindMaximumRequest{Number: n}); err != nil {
+//				close(waitc)
+//				log.Fatalf("error sending int to server: [ %v ]", err)
+//			}
+//		}
+//		if err := fMC.CloseSend(); err != nil {
+//			close(waitc)
+//			log.Fatalf("error closeSend: [ %v ]", err)
+//		}
+//	}()
+//
+//	var rIH []int32
+//	go func() {
+//		//	Receiving stream
+//		for {
+//			rI, err := fMC.Recv()
+//			if err == io.EOF {
+//				break
+//			}
+//			if err != nil {
+//				close(waitc)
+//				log.Fatalf("error recieving data: [ %v ]", err)
+//			}
+//			fmt.Printf("Recieved Maximum int: [ %v ]\n", rI.GetCurrentMaximum())
+//			rIH = append(rIH, rI.GetCurrentMaximum())
+//		}
+//		close(waitc)
+//	}()
+//	<-waitc
+//
+//	fmt.Printf("All maximums were: %v\n", rIH)
+//}
 
 //func doClientStreaming(c calculatorpb.CalculatorServiceClient) (err error) {
 //	s, err := c.ComputeAverage(context.Background())
