d8351236db56f47f29d546f25764b22862e66a9b
George Baronheid <baronheid.george@gmail.com>
2021-12-07 20:20:36 -0300

    Basic server setup

--- blog/blog_server/server.go
+++ blog/blog_server/server.go
@@ -1,1 +1,51 @@
-package blog_server
+package main
+
+import (
+    "fmt"
+    "google.golang.org/grpc"
+    "grpc-go-course/blog/blogpb"
+    "log"
+    "net"
+    "os"
+    "os/signal"
+)
+
+type server struct {
+    blogpb.UnimplementedBlogServiceServer
+}
+
+func main() {
+    //If we crash the go code we get the file name and line number
+    log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+    s := grpc.NewServer()
+    lis, err := net.Listen("tcp", "0.0.0.0:50051")
+    if err != nil {
+        log.Fatalf("Failed to listen: %v", err)
+    }
+
+    blogpb.RegisterBlogServiceServer(s, &server{})
+
+    go func() {
+        fmt.Println("[ blog ] Starting server up!")
+        if err := s.Serve(lis); err != nil {
+            log.Fatalf("Failed to server: %v", err)
+        }
+    }()
+
+    // Waits for control + c to exit
+    ch := make(chan os.Signal, 1)
+    signal.Notify(ch, os.Interrupt)
+
+    //    Block until signal is recieved
+    <-ch
+    fmt.Println("Stopping the server")
+    s.Stop()
+    fmt.Println("Stopping the listener")
+    err = lis.Close()
+    if err != nil {
+        log.Fatalf("Error closing listener: [ %v ]", err)
+    }
+    fmt.Println("Graceful shutdown done")
+
+}
