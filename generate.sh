58cfeb9a4204d87bb97bd7a3741abc466c9fc975
George Baronheid <baronheid.george@gmail.com>
2021-12-07 20:11:36 -0300

    First proto generation

--- generate.sh
+++ generate.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 #Generate pb for greet.proto
-protoc greet/greetpb/greet.proto --go_out=. --go-grpc_out=.
+#protoc greet/greetpb/greet.proto --go_out=. --go-grpc_out=.
 
 #protoc sum/sumpb/sum.proto --go_out=. --go-grpc_out=.
 
 #protoc prime/primepb/primes.proto --go_out=. --go-grpc_out=.
 
 #protoc calculator/calculatorpb/calculator.proto --go_out=. --go-grpc_out=.
+
+protoc blog/blogpb/blog.proto --go_out=. --go-grpc_out=.
+
