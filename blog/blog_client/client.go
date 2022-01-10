04f15488a84e8f5d2f103630aab8df1a0f90ad4e
George Baronheid <baronheid.george@gmail.com>
2022-01-10 19:34:07 -0300

    Created client for blog service and now adding read rpc service

--- blog/blog_client/client.go
+++ blog/blog_client/client.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"grpc-go-course/blog/blogpb"
+	"log"
+)
+
 func main() {
 
+	opts := grpc.WithInsecure()
+
+	conn, err := grpc.Dial("localhost:50051", opts)
+	if err != nil {
+		log.Fatalf("Dial error: %v", err)
+	}
+
+	defer func(conn *grpc.ClientConn) {
+		err := conn.Close()
+		if err != nil {
+			log.Fatalf("Error closing connection inside defer: [ %v ]", err)
+		}
+	}(conn)
+
+	c := blogpb.NewBlogServiceClient(conn)
+
+	in := blogpb.Blog{
+		AuthorId: "George",
+		Title:    "Blog do Jorjinho",
+		Content:  "Aqui tem um belo de um conteúdo",
+	}
+
+	blog, err := c.CreateBlog(context.Background(), &blogpb.CreateBlogRequest{Blog: &in})
+	if err != nil {
+		log.Fatalf("Unexpected error: [ %v ]", err)
+	}
+
+	fmt.Printf("Blog has bem created: [ %v ]", blog.GetBlog())
 }
